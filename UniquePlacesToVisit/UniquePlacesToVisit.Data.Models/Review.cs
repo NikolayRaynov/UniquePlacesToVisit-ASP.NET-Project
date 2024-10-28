345252da29ef2c81cbf81e5139b9ff3d45ea6258
NikolayRaynov <niksan.nr@gmail.com>
2024-10-28 21:08:30 +0200

    Add "Review" model and rename "User" model on "ApplicationUser"

---
+++ UniquePlacesToVisit/UniquePlacesToVisit.Data.Models/Review.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniquePlacesToVisit.Data.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public string ReviewText { get; set; } = null!;
+
+        public int Rating { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public int AttractionId { get; set; }
+
+        [ForeignKey(nameof(AttractionId))]
+        public Attraction Attraction { get; set; } = null!;
+
+        public string UserId { get; set; } = null!;
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
+    }
+
+}
