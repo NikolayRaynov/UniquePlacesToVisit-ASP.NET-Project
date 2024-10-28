cfd6cf4a0f9f0c8a21d0269462d1d32ec9544e3e
NikolayRaynov <niksan.nr@gmail.com>
2024-10-28 20:57:49 +0200

    Add Attraction model

---
+++ UniquePlacesToVisit/UniquePlacesToVisit.Data.Models/Attraction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniquePlacesToVisit.Data.Models
+{
+    public class Attraction
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+        public string Location { get; set; } = null!;
+
+        public int CityId { get; set; }
+
+        [ForeignKey(nameof(CityId))]
+        public City City { get; set; } = null!;
+    }
+}
